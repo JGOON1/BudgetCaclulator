246faebfdb8640c810ccbf7c530a423cb9bcb3a5
Shalom Lee <slee@talentpath.com>
2021-12-10 10:34:45 -0600

    update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ expEntry.addEventListener('click', (e) => {
         expenseString.placeholder = "Please input a budget first";
         expenseNumber.placeholder = "Please input a budget first";
     } else createNewExpense(expenseString.value, expenseNumber.value)
-    // console.log(expenseNumber.value)
+
 })
 
 
@@ -143,72 +143,3 @@ function updateBalance() {
 
 
 
-
-
-
-
-// function sumArray() {
-//     for (let i = 0; i < expenses.length; i++) {
-//         sum += expenses[i]
-//       }
-//    return sum 
-// }
-// //   document.write( "javascript- Sum of the array value is :- " + sum ); 
-// //we want remainig budgte to be budget minus the sum of the expenses array
-
-
-
-// // expense.addEventListener('input', (event) => {
-// //     expense.value = event.target.value
-// //     console.log(expense.value)
-// // })
-
-// // expense.forEach(cost => {
-// //     cost.addEventListener('input', (e) => {
-// //         expense.value = e.target.value
-// //     })
-// // })
-
-// //sets budget amount
-// budgetAmount.addEventListener('input', (event) => {
-//     budgetAmount.value = event.target.value;
-//     console.log(budgetAmount.value)
-// })
-
-// newField.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     index = 0 
-
-//     const newExpense = document.createElement('div')
-//     newExpense.classList.add('expenseList')
-//     const newExpenseHTML = `<input type="string" name="description" class="description" 
-//      placeholder="Expense name"><input type="number" name="expense" class="expense${index}" min="0" placeholder="Expense Cost"> 
-//    </br>`   
-//     index++
-//     expenses.push(expense.value)
-//     console.log(expenses)
-
-//     newExpense.innerHTML = newExpenseHTML
-//     budgetForm.appendChild(newExpense)
-    
-// })
-
-
-// // newField.addEventListener('click', (event) => {
-// //     event.preventDefault();
-// //     const budgetDisplayHTML = `Remaining Budget: ${net}`
-// //     budgetDisplay.innerHTML = budgetDisplayHTML
-
-// // })
-
-
-// calculate.addEventListener('click', (event) => {
-//     event.preventDefault();
-
-//     sumArray()
-//     const budgetDisplayHTML = `Remaining Budget: ${net}`
-//     budgetDisplay.innerHTML = budgetDisplayHTML
-
-// })
-
-// modal popup
